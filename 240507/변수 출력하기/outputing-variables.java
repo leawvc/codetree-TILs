081b31c2caea502cefbb70653088dd1e5c990383
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-07 15:15:25 +0900

    Title: 변수 출력하기, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

--- 240507/변수 출력하기/outputing-variables.java
+++ 240507/변수 출력하기/outputing-variables.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int a = 3;
         String b = "C";
-        System.out.printf("%d...%c",a,b);
+        System.out.printf("%d...%s",a,b);
     }
 }
