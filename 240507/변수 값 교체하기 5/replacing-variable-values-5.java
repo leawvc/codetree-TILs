c0285d62ae987d84f382bdab170e0a384e96432e
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-07 16:13:12 +0900

    Title: 변수 값 교체하기 5, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240507/변수 값 교체하기 5/replacing-variable-values-5.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 6;
+        b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
