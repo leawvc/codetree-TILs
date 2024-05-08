bc25b2e2dd8d14ae84175587a6f31fb82e0d370c
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-08 15:19:04 +0900

    Title: 정수 입력받아 계산, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240508/정수 입력받아 계산/calculation-by-inputting-an-integer.java
+++ 240508/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(system.in);
+        Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.print(a * 2 + 3);
     }
