a0e83f583da9650d5f766953ac989b40a9d53eaa
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-08 13:59:48 +0900

    Title: 데이터 교환, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

--- 240508/데이터 교환/exchange-data.java
+++ 240508/데이터 교환/exchange-data.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 6;
-        int c = 7;
-        int temp, temp1;
+        // 변수 선언
+        int a = 5, b = 6, c = 7;
 
-        temp = b;
-        temp1 = c;
-        b = a;
-        c = temp;
-        a = temp1;
+        // 교체
+        int temp = a;
+        a = c;
+        c = b;
+        b = temp;
 
+        // 출력
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
