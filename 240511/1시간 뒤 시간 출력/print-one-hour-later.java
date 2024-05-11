f987a0893cfd087175544a5ac944481be78988f2
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-11 22:06:24 +0900

    Title: 1시간 뒤 시간 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240511/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter(":"); // : 기준으로 잘라서 입력 받겠다는 뜻
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(h + "\n" + m);
+}
