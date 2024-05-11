17b7daec7ba9556ed128112f443aacdc2d060035
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-11 21:38:17 +0900

    Title: 문자 받아 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240511/문자 받아 출력/enter-char-and-print.java
+++ 240511/문자 받아 출력/enter-char-and-print.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next().charAt(0);
+        Char s = sc.next().charAt(0);
         System.out.print(s);
         // 여기에 코드를 작성해주세요.
     }
