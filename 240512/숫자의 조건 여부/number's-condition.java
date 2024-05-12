5b2bd6a93ad0ec8b94740801b8a0050346338f0c
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-12 15:36:27 +0900

    Title: 숫자의 조건 여부, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240512/숫자의 조건 여부/number's-condition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(113 <= a){
+            System.out.print(1);
+        }else{
+            System.out.print(0);
+        }
+    }
+}
