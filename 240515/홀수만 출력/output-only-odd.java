ce8644f6be234af20094f7478c0d25e49ab672b3
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-15 22:05:54 +0900

    Title: 홀수만 출력, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240515/홀수만 출력/output-only-odd.java
+++ 240515/홀수만 출력/output-only-odd.java
@@ -9,10 +9,8 @@ public class Main {
         a = sc.nextInt();
         b = sc.nextInt();
 
-        for(int i = a; i <= b; i++){
-            if(i % 2 == 1){
-                System.out.print(i + " ");
-            }
+        for(int i = a; i <= b; i += 2){
+            System.out.print(i + " ");
         }
     }
 }
