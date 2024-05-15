bb80442aa2950aa79c2b251631a8d78b3791529b
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-15 13:13:40 +0900

    Title: 코로나 메뉴얼, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240515/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+        char a_t, b_t, c_t;
+        
+
+        a_t = sc.next().charAt(0);
+        a = sc.nextInt();
+
+        b_t = sc.next().charAt(0);
+        b = sc.nextInt();
+
+        c_t = sc.next().charAt(0);
+        c = sc.nextInt();
+        
+        if(a_t == 'Y' && a >= 37) {
+            if(b_t == 'Y' && b >= 37)
+                System.out.println("E");
+            else if(c_t == 'Y' && c >= 37)
+                System.out.println("E");
+            else
+                System.out.println("N");
+        }
+        else if(b_t == 'Y' && b >= 37){
+            if(c_t == 'Y' && c >= 37)
+                System.out.println("E");
+            else
+                System.out.println("N");
+        }
+    }
+}
