1502ac3ea7d1a96dc04252776db2ebde7f629a70
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-16 15:58:08 +0900

    Title: 3의 배수 출력하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240516/3의 배수 출력하기/print-multiple-of-number-three.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+        int i = 3;
+
+        a = sc.nextInt();
+
+        while(i <= a){
+            System.out.print(i +" ");
+            i += 3;
+        }
+    }
+}
