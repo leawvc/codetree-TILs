c46f35d71e4c1b6c599f6cdb8ec0212a7a825189
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-17 22:52:09 +0900

    Title: 문자 출력하기, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240517/문자 출력하기/print-text.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a;
+
+        a = sc.next();
+
+        for(int i = 0; i < 8; i++){
+            System.out.print(a);
+        }
+    }
+}
