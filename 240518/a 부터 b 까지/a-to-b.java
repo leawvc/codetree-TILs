f051fc99ed673a9347e83e08fd42f3db4a3a018a
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-18 00:21:34 +0900

    Title: a 부터 b 까지, Time: 228ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240518/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        for(int i = a; i <= b;){
+            if(i % 2 != 0){
+                System.out.print(i +" ");
+                i *= 2;
+            }else{
+                System.out.print(i +" ");
+                i += 3;
+            }
+        }
+    }
+}
