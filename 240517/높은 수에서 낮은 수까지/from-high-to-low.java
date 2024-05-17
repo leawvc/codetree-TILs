cea97ebd6ca6bfabccb34f0bb3e8f13bf2fe8c48
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-17 23:36:52 +0900

    Title: 높은 수에서 낮은 수까지, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240517/높은 수에서 낮은 수까지/from-high-to-low.java
+++ 240517/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -9,12 +9,12 @@ public class Main {
         a = sc.nextInt();
         b = sc.nextInt();
 
-        if(a < b){
-            for(int i = a; a <= b; a++){
+        if(a > b){
+            for(int i = a; a >= b; a--){
                 System.out.print(a + " ");
             }
         }else{
-            for(int i = b; b <= a; b++){
+            for(int i = b; b >= a; b--){
                 System.out.print(b + " ");
             }
         }
