3ce5d1245c60fb6cc637acb05fba5bf741708602
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-17 22:50:49 +0900

    Title: 짝수만 출력 2, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240517/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        while(a >= b){
+            System.out.print(a + " ");
+            a -= 2;
+        }
+    }
+}
