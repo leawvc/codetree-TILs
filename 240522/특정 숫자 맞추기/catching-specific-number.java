06685e2acbb398a52238125ea9160e9a5a9ec2ea
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-22 21:52:43 +0900

    Title: 특정 숫자 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240522/특정 숫자 맞추기/catching-specific-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+
+        for (int i = 1; i <= 100; i++) {
+            a = sc.nextInt();
+            if(a == 25){
+                System.out.println("Good");
+            }else if(a > 25){
+                System.out.println("Lower");
+            }else
+                System.out.println("Higher");
+        }
+    }
+}
