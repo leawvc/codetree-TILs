87e8d13afbe4ff836add900ff6f6497542892860
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-24 11:52:37 +0900

    Title: 별표 출력하기 3, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240524/별표 출력하기 3/print-star-3.java
+++ 240524/별표 출력하기 3/print-star-3.java
@@ -7,9 +7,9 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2 * i + 1; j++){
-                System.out.print(" ");
-            }   
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
             for (int j = 2 * n - 1; j > 2 * i; j--){
                 System.out.print("* ");
             }
