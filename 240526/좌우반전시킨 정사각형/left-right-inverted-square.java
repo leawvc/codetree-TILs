4839222c0a33740e1f29317e757a651404e50b29
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-26 18:29:40 +0900

    Title: 좌우반전시킨 정사각형, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240526/좌우반전시킨 정사각형/left-right-inverted-square.java
+++ 240526/좌우반전시킨 정사각형/left-right-inverted-square.java
@@ -8,7 +8,7 @@ public class Main {
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print((4 - j) * (i + 1) + " ");
+                System.out.print((n - j) * (i + 1) + " ");
             }
             System.out.println();
         }
