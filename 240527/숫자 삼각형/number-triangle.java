8f839fd10031f829437b1465c4e080701351206b
leawvc <43610417+leawvc@users.noreply.github.com>
2024-05-27 16:06:14 +0900

    Title: 숫자 삼각형, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/숫자 삼각형/number-triangle.java
+++ 240527/숫자 삼각형/number-triangle.java
@@ -8,7 +8,7 @@ public class Main {
 
         for (int j = 1; j <= n; j++) {
             for (int i = 1; i <= j; i++) {
-                System.out.print(i);
+                System.out.print(i +" ");
             }
             System.out.println();
         }
