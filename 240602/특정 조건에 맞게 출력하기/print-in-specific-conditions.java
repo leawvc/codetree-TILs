03f682b2c21e8312f4b4ba096ac49d4caf30912c
leawvc <43610417+leawvc@users.noreply.github.com>
2024-06-02 18:01:52 +0900

    Title: 특정 조건에 맞게 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240602/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
+++ 240602/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
         int [] arr = new int[100];
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < 100; i++){
             arr[i] = sc.nextInt();
         }
-        for(int i = 0; i < n; i ++){
+        for(int i = 0; i < 100; i ++){
             if(arr[i] == 0)
                 break;
             if(arr[i] % 2 != 0)
