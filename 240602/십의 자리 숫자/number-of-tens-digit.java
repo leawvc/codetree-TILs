de9bb9295866fe8d1f9388fe94e381a5a7895a1a
leawvc <43610417+leawvc@users.noreply.github.com>
2024-06-02 18:47:43 +0900

    Title: 십의 자리 숫자, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240602/십의 자리 숫자/number-of-tens-digit.java
+++ 240602/십의 자리 숫자/number-of-tens-digit.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
+        int[] arr = new int[100];
         int[] countArr = new int[10];
 
         for (int i = 0; i < 100; i++) {
