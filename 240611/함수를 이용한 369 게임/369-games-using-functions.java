bbd0d07e211208eb26ce14b0586415062ec64bbb
leawvc <43610417+leawvc@users.noreply.github.com>
2024-06-11 21:47:29 +0900

    Title: 함수를 이용한 369 게임, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240611/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isMagicNumber(int n) {
+        if(n % 3 == 0)
+            return true;
+        while(n != 0){
+            if((n % 10) % 3 == 0)
+                return true;
+            n /= 10;
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int cnt = 0;
+
+        for(int i = n; i <= m; i++)
+            if(isMagicNumber(i))
+                cnt++;
+
+        System.out.println(cnt);
+    }
+}
