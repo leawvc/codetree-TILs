c88e7423ecb4a8cab5e94406d806b059e86197e1
leawvc <43610417+leawvc@users.noreply.github.com>
2024-06-11 22:01:13 +0900

    Title: 함수를 이용한 윤년 판별, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240611/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 240611/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static boolean isOnjeonsu(int n) {
         if(n % 100 == 0 && n % 400 != 0)
-            return true;
+            return false;
         if(n % 4 == 0)
             return true;
         return false;
