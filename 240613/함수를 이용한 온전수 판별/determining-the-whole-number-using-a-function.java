52fcad997a5f104b53cae19ce637682790ed5cea
leawvc <43610417+leawvc@users.noreply.github.com>
2024-06-13 21:48:29 +0900

    Title: 함수를 이용한 온전수 판별, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240613/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
+++ 240613/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int onjeon(int a, int b){
-        int cnt = 0;
-        for(int i = a; i <= b; i++){
-            if(i % 2 != 0 && i % 10 != 5 && (i % 3 != 0 && i % 9 == 0))
-                cnt++; 
-        }
-        return cnt;
+    public static boolean onjeon(int a){
+        if(a % 2 == 0 || a % 10 == 5 || (a % 3 == 0 && a % 9 != 0))
+            return false;
+        return true;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int b = sc.nextInt();
+        int cnt = 0;
 
-        System.out.print(onjeon(a, b));
+        for(int i = a; i <= b; i++){
+            if(onjeon(a))
+                cnt++;
+        }
+        System.out.print(cnt);
     }
 }
