4a607e728e3439db48af01358e7bf9b0f496df2c
leawvc <43610417+leawvc@users.noreply.github.com>
2024-06-13 22:10:40 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240613/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 240613/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -1,51 +1,42 @@
 import java.util.Scanner;
 
 public class Main {
-    public static boolean onjeon(int[] arr, int[] arr1) {
-        int arrLength = arr.length;
-        int arr1Length = arr1.length;
-        
-        // Loop through the main array to find the start of the subarray
-        for (int i = 0; i <= arrLength - arr1Length; i++) {
-            // Check if the subarray matches
-            boolean match = true;
-            for (int j = 0; j < arr1Length; j++) {
-                if (arr[i + j] != arr1[j]) {
-                    match = false;
-                    break;
-                }
+    public static boolean onjeon(int []arr, int []arr1){
+        int cnt = 0;
+        for(int i = 0; i < arr.length -1; i++){
+            if(arr[i] == arr1[0]){
+                break;
             }
-            if (match) {
-                return true;
-            }
+            cnt++;
         }
-        return false;
+        for(int i = 0; i < arr.length - arr1.length; i++){
+            if(arr[cnt + i] != arr1[i])
+                return false;
+        }
+        return true;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int[] arr = new int[a];
-        int[] arr1 = new int[b];
+        int [] arr = new int [a];
+        int [] arr1 = new int [b];
 
-        for (int i = 0; i < a; i++) {
+        for(int i = 0; i < a; i++){
             arr[i] = sc.nextInt();
         }
-        for (int i = 0; i < b; i++) {
+        for(int i = 0; i < b; i++){
             arr1[i] = sc.nextInt();
         }
-
-        // Check if arr1 is a subarray of arr or vice versa
-        if (a >= b) {
-            if (onjeon(arr, arr1))
+        if(arr.length >= arr1.length){
+            if(onjeon(arr, arr1))
                 System.out.print("Yes");
             else
                 System.out.print("No");
-        } else {
-            if (onjeon(arr1, arr))
+        }else{
+            if(onjeon(arr1, arr))
                 System.out.print("Yes");
             else
                 System.out.print("No");
