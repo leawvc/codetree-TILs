bb2cded460a614fae098e02d805f9fac6081adcb
leawvc <43610417+leawvc@users.noreply.github.com>
2024-06-17 17:16:44 +0900

    Title: 코드네임, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240617/코드네임/code-name.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class CodeName {
+    char code;
+    int point;
+
+    public CodeName() {
+        this.code = 0;
+        this.point = 0;
+    }
+
+    public CodeName(char code, int point){
+        this.code = code;
+        this.point = point;
+    }
+};
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+        CodeName[] CodeNames = new CodeName[5];
+        int min = 100;
+        for(int i = 0; i < 5; i++) {
+            char code = sc.next().charAt(0);
+            int point = sc.nextInt();
+            if(point <= min)
+                min = i;
+            CodeNames[i] = new CodeName(code, point);
+        }
+
+        CodeName CodeName3 = CodeNames[min];
+        System.out.print(CodeName3.code + " " + CodeName3.point);
+    }
+}
