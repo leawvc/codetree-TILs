5ce60fc0fd44864969889a24844772c9132f7418
leawvc <43610417+leawvc@users.noreply.github.com>
2024-06-17 21:05:54 +0900

    Title: 사는 지역, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240617/사는 지역/where-live.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Resion{
+
+    String name, bun, place;
+
+    public Resion(){
+        this.name = "0";
+        this.bun = "0";
+        this.place = "0";
+    }
+
+    public Resion(String name, String bun, String place){
+        this.name = name;
+        this.bun = bun;
+        this.place = place;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Resion [] resions = new Resion[5];
+
+        for(int i = 0; i < n; i++){
+            String name = sc.next();
+            String bun = sc.next();
+            String place = sc.next();
+
+            resions[i] = new Resion(name, bun, place);
+        }
+
+        int min = 0;
+        for(int i = 1; i < n; i++){
+            if(resions[i].name.compareTo(resions[min].name) > 0)
+                min = i;
+        }
+        System.out.println("name " + resions[min].name);
+        System.out.println("addr " + resions[min].bun);
+        System.out.println("city " + resions[min].place);
+    }
+}
