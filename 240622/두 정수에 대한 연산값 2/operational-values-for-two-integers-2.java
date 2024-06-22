7e81769bf966259299b0b8b89cac4c00d4a06844
leawvc <43610417+leawvc@users.noreply.github.com>
2024-06-22 23:12:49 +0900

    Title: 두 정수에 대한 연산값 2, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240622/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Wrap{
+    int value;
+
+    public Wrap(int value){
+        this.value = value;
+    }    
+}
+public class Main {
+    public static int a, b;
+
+    public static void swap(Wrap n, Wrap m){
+        if(n.value < m.value){
+            n.value += 10;
+            m.value *= 2;
+        }else{
+            n.value *= 2;
+            m.value += 10;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        Wrap n = new Wrap(a);
+        Wrap m = new Wrap(b);
+
+        swap(n, m);
+        a = n.value;
+        b = m.value;
+        System.out.print(a + " " + b);
+    }
+}
