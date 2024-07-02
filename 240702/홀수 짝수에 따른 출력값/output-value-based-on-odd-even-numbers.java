56cbf90ae8eb7d24bb095f7dd518d7301094226e
leawvc <43610417+leawvc@users.noreply.github.com>
2024-07-02 22:30:57 +0900

    Title: 홀수 짝수에 따른 출력값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240702/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int fact(int n, int cnt){
+        if(n == 1)
+            return cnt;
+        else if(n % 2 == 1)
+            return fact(n - 2, cnt) + n;
+        else
+            return fact(n - 2, cnt) + n;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.print(fact(n, 1));
+    }
+}
