e68ea28f5d4244e2d027109f38349123f6220ed7
leawvc <43610417+leawvc@users.noreply.github.com>
2024-07-12 16:22:06 +0900

    Title: 중앙값 계산 2, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240712/중앙값 계산 2/get-median-2.java
+++ 240712/중앙값 계산 2/get-median-2.java
@@ -6,15 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       int n = sc.nextInt();
-        int[] num = new int[n];
+        int n = sc.nextInt();
+        int [] num = new int[n];
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             num[i] = sc.nextInt();
-            if (i % 2 == 0) {
-                int[] temp = Arrays.copyOf(num, i + 1);
-                Arrays.sort(temp);
-                System.out.print(temp[i / 2] + " ");
+            if(i % 2 == 0){
+                Arrays.sort(num, 0, i + 1);
+                System.out.print(num[i / 2] + " ");
             }
         }
     }
