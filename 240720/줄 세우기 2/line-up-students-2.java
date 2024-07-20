860cd4c5279d9c6a144aad2712ec06ea6db5deae
leawvc <43610417+leawvc@users.noreply.github.com>
2024-07-20 21:59:57 +0900

    Title: 줄 세우기 2, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240720/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Distance implements Comparable<Distance>{
+    int h, w, index;
+
+    public Distance(int h, int w, int index){
+        this.h = h;
+        this.w = w;
+        this.index = index;
+    }
+    @Override 
+    public int compareTo(Distance distance){
+        if(this.h == distance.h)
+            return this.w - distance.w;
+        return this.h - this.h;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Distance [] distances = new Distance[n];
+
+        for(int i = 0; i < n; i++){
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            distances[i] = new Distance(h, w, i + 1);
+        }
+        Arrays.sort(distances);
+        for(int i = 0; i < n; i++)
+            System.out.println(distances[i].h + " " + distances[i].w + " " + distances[i].index + " ");
+    }
+}
