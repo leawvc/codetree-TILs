a8173256e6f014e256c8292824e5cf70bad5904c
leawvc <43610417+leawvc@users.noreply.github.com>
2024-07-21 21:59:28 +0900

    Title: Date to Date, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240721/Date to Date/date-to-date.java
+++ 240721/Date to Date/date-to-date.java
@@ -1,29 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int getPastDays(int m, int d){
+        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int value = 0;
+        
+        for(int i = 0; i < m; i++)
+            value += num_of_days[i];
+        return value + d;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-        int elapsedDays = 1;
 
-        //                                1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
-        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        while(true) {
-            if(m1 == m2 && d1 == d2)
-                break;
-        
-            elapsedDays++;
-            d1++;
-        
-            if(d1 > num_of_days[m1]) {
-                m1++;
-                d1 = 1;
-            }
-        }
-        
-        System.out.print(elapsedDays);
+        System.out.print(getPastDays(m2, d2) - getPastDays(m1, d1) + 1);
     }
 }
