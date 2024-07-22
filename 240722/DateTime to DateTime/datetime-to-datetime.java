50e9bf9f3997c2ee93b7033709d95c1734ff77e6
leawvc <43610417+leawvc@users.noreply.github.com>
2024-07-22 21:52:59 +0900

    Title: DateTime to DateTime, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240722/DateTime to DateTime/datetime-to-datetime.java
+++ 240722/DateTime to DateTime/datetime-to-datetime.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static int getPastDays(int a, int b, int c){
-        if(a == 11 && b <= 11 && c < 11)
+        if((a == 11 && b < 11) || (b == 11 && c < 11))
             return -1;
         return (a * 1440 + b * 60 + c) - (11 * 1440 + 11 * 60 + 11);
     }
