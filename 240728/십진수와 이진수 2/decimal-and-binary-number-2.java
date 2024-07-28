ef93fdd17b00c5258f789655d410f0d05520e793
leawvc <43610417+leawvc@users.noreply.github.com>
2024-07-28 21:05:04 +0900

    Title: 십진수와 이진수 2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240728/십진수와 이진수 2/decimal-and-binary-number-2.java
+++ 240728/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
         int sum = 0;
         int cnt = 1;
-        while(n != 0){
+        while(n > 0){
             sum += n % 10 * cnt;
             n /= 10;
             cnt *= 2;
@@ -15,7 +15,7 @@ public class Main {
         sum *= 17;
         int num = 0;
         cnt = 1;
-        while(sum != 0){
+        while(sum > 0){
             num += sum % 2 * cnt;
             sum /= 2;
             cnt *= 10;
