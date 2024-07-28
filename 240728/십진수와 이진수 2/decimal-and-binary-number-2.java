719fcaef7f61809503a535e9c45d2db25cacf077
leawvc <43610417+leawvc@users.noreply.github.com>
2024-07-28 21:11:11 +0900

    Title: 십진수와 이진수 2, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240728/십진수와 이진수 2/decimal-and-binary-number-2.java
+++ 240728/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -4,17 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 입력을 2진수 문자열로 받기
-        String binaryInput = sc.next();
-        int n = Integer.parseInt(binaryInput, 2);  // 2진수를 10진수로 변환
-
-        // sum 계산하기
-        int sum = n * 17;
-
-        // sum을 2진수 문자열로 변환
-        String binaryOutput = Integer.toBinaryString(sum);
-
-        // 결과 출력
-        System.out.print(binaryOutput);
+        String binary = sc.next();
+        int sum = 0;
+        for(int i = 0; i < binary.length(); i++)
+            sum = sum * 2 + (binary.charAt(i) - '0');
+        sum *= 17;
+        int num = 0;
+        int cnt = 1;
+        while(sum > 0){
+            num += sum % 2 * cnt;
+            sum /= 2;
+            cnt *= 10;
+        }
+        System.out.print(num);
     }
 }
