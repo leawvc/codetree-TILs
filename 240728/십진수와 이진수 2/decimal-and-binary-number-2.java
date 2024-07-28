91fe996af22cc14cae9f6c6d5cce0843bc50476a
leawvc <43610417+leawvc@users.noreply.github.com>
2024-07-28 21:07:54 +0900

    Title: 십진수와 이진수 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240728/십진수와 이진수 2/decimal-and-binary-number-2.java
+++ 240728/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -4,18 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // 입력을 10진수 정수로 받기
         int n = sc.nextInt();
+
         int sum = 0;
         int cnt = 1;
-        while(n > 0){
+        while (n != 0) {
             sum += n % 10 * cnt;
             n /= 10;
             cnt *= 2;
         }
         sum *= 17;
-        long num = 0;
+
+        long num = 0;  // long으로 변경
         cnt = 1;
-        while(sum > 0){
+        while (sum != 0) {
             num += sum % 2 * cnt;
             sum /= 2;
             cnt *= 10;
