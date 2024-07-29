11f378860e380d3937d8cf04d333faec77859e5c
leawvc <43610417+leawvc@users.noreply.github.com>
2024-07-29 22:41:09 +0900

    Title: 진수 to 진수, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240729/진수 to 진수/transformation-of-number-system.java
+++ 240729/진수 to 진수/transformation-of-number-system.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int [] arr;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -10,13 +11,14 @@ public class Main {
         int sum = 0;
         for(int i = 0; i < n.length(); i++)
             sum = sum * a + (n.charAt(i) - '0');
-        int cnt = 1;
         int num = 0;
+        arr = new int [20];
         while(sum > 0){
-            num += sum % b * cnt;
+            arr[num] = sum % b;
             sum /= b;
-            cnt *= 10;
+            num++;
         }
-        System.out.print(num);
+        for(int i = num - 1; i >= 0; i--)
+            System.out.print(arr[i]);
     }
 }
