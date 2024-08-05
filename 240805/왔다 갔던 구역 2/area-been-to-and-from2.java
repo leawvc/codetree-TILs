5fc2beb67a94d67f1af5d3412794584d506fe7ae
leawvc <43610417+leawvc@users.noreply.github.com>
2024-08-05 21:52:33 +0900

    Title: 왔다 갔던 구역 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240805/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240805/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,25 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int offset = 2000;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int offset = 2000;
         int n = sc.nextInt();
         int [] arr = new int [4001];
-
+        
         for(int i = 0; i < n; i++){
             int a = sc.nextInt();
             String b = sc.next();
-            if(b.equals('L'))
-                offset -= a;
-            else
-                offset += a;
-            for(int j = 0; j <= a; j++)
-                arr[offset + j]++;
+            if(b.equals("L")){
+                for(int j = 0; j < a; j++){
+                    offset--;
+                    arr[offset]++;
+                }
+            }
+            else{
+                for(int j = 0; j < a; j++){
+                    offset++;
+                    arr[offset]++;
+                }
+            }
         }
         int cnt = 0;
-        for(int i = 0; i < 4001; i++){
+        for(int i = 0; i < arr.length; i++){
             if(2 <= arr[i])
                 cnt++;
         }
