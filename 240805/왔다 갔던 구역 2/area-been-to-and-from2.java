3172c2778ee2291595ef015f3c56a7c27a1860a4
leawvc <43610417+leawvc@users.noreply.github.com>
2024-08-05 21:58:59 +0900

    Title: 왔다 갔던 구역 2, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240805/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240805/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -5,25 +5,32 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] arr = new int[4001];
-        int offset = 2000;
+        int[] arr = new int[4001]; // 2000 ~ -2000 (총 4001 크기)
+        int offset = 2000; // 배열의 중간을 2000으로 설정
+
+        // 현재 위치
+        int cur = offset;
 
         for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            String b = sc.next();
+            int distance = sc.nextInt();
+            String direction = sc.next();
 
-            if (b.equals("L")) {
-                for (int j = 0; j < a; j++) {
-                    arr[offset]++;
-                    offset--;
+            if (direction.equals("L")) {
+                // 왼쪽으로 이동
+                for (int j = 0; j < distance; j++) {
+                    arr[cur]++; // 현재 위치의 방문 횟수 증가
+                    cur--; // 위치 이동
                 }
             } else {
-                for (int j = 0; j < a; j++) {
-                    arr[offset]++;
-                    offset++;
+                // 오른쪽으로 이동
+                for (int j = 0; j < distance; j++) {
+                    arr[cur]++; // 현재 위치의 방문 횟수 증가
+                    cur++; // 위치 이동
                 }
             }
         }
+
+        // 2번 이상 지나간 영역의 크기를 계산
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= 2) {
