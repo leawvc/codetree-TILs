b14d784437e09c93359e44de92cb8465211fbc00
leawvc <43610417+leawvc@users.noreply.github.com>
2024-08-05 21:55:51 +0900

    Title: 왔다 갔던 구역 2, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240805/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240805/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -5,28 +5,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] arr = new int[4001]; // 배열 크기: -2000 ~ 2000을 표현하기 위한 크기
-        int currentPosition = 2000; // 배열의 중앙을 현재 위치 0으로 설정
+        int[] arr = new int[4001];
+        int offset = 2000;
 
-        // 명령어를 읽어 이동 경로를 기록합니다.
         for (int i = 0; i < n; i++) {
             int a = sc.nextInt();
-            String direction = sc.next();
+            String b = sc.next();
 
-            if (direction.equals("L")) {
+            if (b.equals("L")) {
                 for (int j = 0; j < a; j++) {
-                    currentPosition--;
-                    arr[currentPosition]++;
+                    offset--;
+                    arr[offset]++;
                 }
             } else {
                 for (int j = 0; j < a; j++) {
-                    currentPosition++;
-                    arr[currentPosition]++;
+                    offset++;
+                    arr[offset]++;
                 }
             }
         }
-
-        // 2번 이상 지나간 구간의 개수를 계산합니다.
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= 2) {
