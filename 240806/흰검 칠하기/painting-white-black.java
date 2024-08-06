452540c50bbae73d59e5a484ae050f6c394efc90
leawvc <43610417+leawvc@users.noreply.github.com>
2024-08-06 22:14:32 +0900

    Title: 흰검 칠하기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240806/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[4001][2];
+        String [] str = new String[4001];
+        int offset = 2000;
+        for (int i = 0; i < str.length; i++) {
+            str[i] = "";
+        }
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            String b = sc.next();
+
+            if (b.equals("L")) {
+                for (int j = 0; j < a; j++) {
+                    offset--;
+                    arr[offset][0]++;
+                    arr[offset][1]++;
+                    str[offset] = "white";
+                }
+            } else {
+                for (int j = 0; j < a; j++) {
+                    arr[offset][0]++;
+                    arr[offset][1] += 3;
+                    str[offset] = "black";
+                    offset++;
+                }
+            }
+        }
+        int white = 0;
+        int black = 0;
+        int gray = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i][0] == 4)
+                gray++;
+            else if(str[i].equals("white"))
+                white++;
+            else if(str[i].equals("black"))
+                black++;
+        }
+        System.out.print(white + " " + black + " " + gray);
+    }
+}
