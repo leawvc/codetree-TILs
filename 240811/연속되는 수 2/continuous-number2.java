dd51642d0c483ff5252cda51e3cb2f058cdb836c
leawvc <43610417+leawvc@users.noreply.github.com>
2024-08-11 22:10:05 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240811/연속되는 수 2/continuous-number2.java
+++ 240811/연속되는 수 2/continuous-number2.java
@@ -5,6 +5,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        if (n == 0) {
+            System.out.print(0);
+            return;
+        }
         int arr [] = new int[n];
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
