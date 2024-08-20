e6cbbb471bd4bd4b6dc0c5623b1dcaa76d78235d
leawvc <43610417+leawvc@users.noreply.github.com>
2024-08-20 21:41:39 +0900

    Title: 만나는 그 순간, Time: 481ms, Memory: 37MB, Status: Passed - CodeTree

--- 240820/만나는 그 순간/the-moment-we-meet.java
+++ 240820/만나는 그 순간/the-moment-we-meet.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,42 +7,34 @@ public class Main {
 
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int a[] = new int[40000];
-        int b[] = new int[40000];
-        int nValue = 0, mValue = 0, cnt = 0;
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
+        int nValue = 0, mValue = 0;
         for(int i = 0; i < n; i++){
             String s = sc.next();
             int num = sc.nextInt();
             for(int j = 0; j < num; j++){
-                if(s.equals("R")){
-                    a[cnt] = nValue;
+                a.add(nValue);
+                if(s.equals("R"))
                     nValue++;
-                }else{
-                    a[cnt] = nValue;
+                else
                     nValue--;
-                }
-                cnt++;
             }
         }
-        cnt = 0;
-        int len = 0;
         for(int i = 0; i < m; i++){
             String s = sc.next();
             int num = sc.nextInt();
             for(int j = 0; j < num; j++){
-                if(s.equals("R")){
-                    b[cnt] = mValue;
+                b.add(mValue);
+                if(s.equals("R"))
                     mValue++;
-                }else{
-                    b[cnt] = mValue;
+                else
                     mValue--;
-                }
-                cnt++;
             }
-            len = cnt;
         }
+        int len = Math.min(a.size(), b.size());
         for(int i = 1; i < len; i++){
-            if(a[i] == b[i]){
+            if(a.get(i).equals(b.get(i))){
                 System.out.print(i);
                 return;
             }
