f824e6cad5795c0db18f4f12fe5b683af0d15503
leawvc <43610417+leawvc@users.noreply.github.com>
2024-08-20 21:09:07 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 186ms, Memory: 11MB, Status: Passed - CodeTree

--- 240820/T를 초과하는 연속 부분 수열/subsequence-above-t.java
+++ 240820/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -4,34 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();  // 수열의 길이
-        int t = sc.nextInt();  // 기준 값
+        int n = sc.nextInt();
+        int t = sc.nextInt();
         int arr[] = new int[n];
 
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();  // 수열 입력
-        }
-
-        int maxLen = 0;  // 최대 길이
-        int currentLen = 0;  // 현재 연속 부분 수열의 길이
-
-        for(int i = 0; i < n; i++) {
-            if(arr[i] > t) {
-                currentLen++;  // t보다 크면 길이 증가
-            } else {
-                // t보다 크지 않으면 최대 길이 갱신 후, 길이 초기화
-                if(currentLen > maxLen) {
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        int maxLen = 0, currentLen = 0;
+        for(int i : arr){
+            if(i > t)
+                currentLen++;
+            else{
+                if(currentLen > maxLen)
                     maxLen = currentLen;
-                }
                 currentLen = 0;
             }
         }
-
-        // 마지막 구간을 처리
-        if(currentLen > maxLen) {
-            maxLen = currentLen;
-        }
-
-        System.out.println(maxLen);  // 최대 길이 출력
+        System.out.println(maxLen);
     }
 }
