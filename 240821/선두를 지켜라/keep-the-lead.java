c19961e5e521a51c47c3be7a78aac08754efce05
leawvc <43610417+leawvc@users.noreply.github.com>
2024-08-21 14:25:31 +0900

    Title: 선두를 지켜라, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240821/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
+        
+        int num = 0;
+        for(int i = 0; i < n; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            for(int j = 0; j < t; j++){
+                num += v;
+                a.add(num);
+            }
+        }
+        num = 0;
+        for(int i = 0; i < m; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            for(int j = 0; j < t; j++){
+                num += v;
+                b.add(num);
+            }
+        }
+        int cnt = 0;
+        for(int i = 0; i < a.size() - 1; i++){
+            if(a.get(i) < b.get(i) && a.get(i + 1) > b.get(i))
+                cnt++;
+            if(a.get(i) > b.get(i) && a.get(i + 1) < b.get(i))
+                cnt++;
+        }
+        if(cnt != 0)
+            System.out.print(cnt);
+        else
+            System.out.print(-1);
+    }
+}
