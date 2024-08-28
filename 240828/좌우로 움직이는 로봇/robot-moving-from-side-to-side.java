8a814eaeb68c00b463cf93ea3e7b85e69824daf4
leawvc <43610417+leawvc@users.noreply.github.com>
2024-08-28 16:48:19 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240828/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static int cnt;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        ArrayList<Integer> A = new ArrayList<>();
+        ArrayList<Integer> B = new ArrayList<>();
+        cnt = 0;
+        for(int i = 0; i < n; i++){
+            int t = sc.nextInt();
+            String d = sc.next();
+            for(int j = 0; j < t; j++){
+                if(d.equals("L")){
+                    A.add(cnt);
+                    cnt--;
+                }else{
+                    A.add(cnt);
+                    cnt++;
+                }
+            }
+        }
+        cnt = 0;
+        for(int i = 0; i < n; i++){
+            int t = sc.nextInt();
+            String d = sc.next();
+            for(int j = 0; j < t; j++){
+                if(d.equals("L")){
+                    B.add(cnt);
+                    cnt--;
+                }else{
+                    B.add(cnt);
+                    cnt++;
+                }
+            }
+        }
+        cnt = 0;
+        for(int i = 1; i < A.size(); i++){
+            if(A.get(i) == B.get(i))
+                cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
